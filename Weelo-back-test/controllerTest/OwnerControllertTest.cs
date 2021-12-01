4878a6c30841d90bfb0f448256c374e858c72066
jemanuelvl <jemanuelvl3@gmail.com>
2021-12-01 15:24:19 -0500

    clean project

---
+++ Weelo-back-test/controllerTest/OwnerControllertTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weelo_back_test.controllerTest
+{
+    [TestFixture]
+    public class OwnerControllertTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+
+    }
+}
